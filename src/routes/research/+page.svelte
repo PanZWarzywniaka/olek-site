7e7f99cf7d3fe5439b9dd468a595628f5125b752
Olek Osikowicz <tupolewcx@gmail.com>
2024-02-11 17:08:25 +0000

    Add research info

--- src/routes/research/+page.svelte
+++ src/routes/research/+page.svelte
@@ -3,14 +3,26 @@
 
 <article>
 	<hgroup>
-		<h1>Research interests</h1>
+		<h1>Research</h1>
 	</hgroup>
 
 	<div class="prose">
 		<p>
-			I am PhD student at the
+			Starting from September 2023, I am PhD student at the
 			<a href="https://www.sheffield.ac.uk/dcs">Department of Computer Science</a>,
 			<a href="https://www.sheffield.ac.uk/">University of Sheffield</a>, Sheffield, UK.
 		</p>
+		<h2>Autonomous Vehicles Testing</h2>
+		<p>
+			I am concentrating my research on scenario reconstruction from real-world accidents for robust
+			verification and validation of ML-enabled automated driving systems (ADS). This involves
+			developing innovative AI testing methodologies.
+		</p>
 	</div>
 </article>
+
+<style>
+	.prose {
+		margin-top: var(--size-4);
+	}
+</style>
